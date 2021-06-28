f0ce80c264863ff3583686a5c9bc1a9bd121d7d5
정우현 <wjddngus102@naver.com>
2021-06-29 00:14:42 +0900

    최종수정일듯

--- myupbit/upbitClient.js
+++ myupbit/upbitClient.js
@@ -119,13 +119,19 @@ async function sellAll() {// 전량매각
 
 async function Armageddon(){ // 아마겟돈(전량 매각하고 새출발)
     sellAll()
+    console.log("전량 매각 완료")
 }
-async function LastGamble(){ // 망하면 10만원남기고 BTC풀매수하고 기도
+async function LastGamble(){ // 망하면 100만원남기고 BTC풀매수하고 기도
     console.log("Last Gamble : " + market);
-    body = await API_buyImmediate(market[0], MyKRWBalance - 100000);
+    body = await API_buyImmediate(market[0], MyKRWBalance - 1000000);
     volume[market] = body.volume
 }
-
+async function Super_Danta(){ // 매수힘 매도힘만보고 무지성 구매
+    console.log("개미들아 힘을 내 ! : " + market);
+    body = await API_buyImmediate(market, 1000000);
+    volume[market] = body.volume
+    // setTimeout(API_sellImmediate(market, volume[market]/2),10000); // 10초뒤 구매량의 절반 매도
+}
 
 volume = {}
 
@@ -135,76 +141,82 @@ async function main() { // 트레이딩 메인(반복문사용)
     moment = 1.0;
 
     ret = await get('http://kali.securekim.com:3082/signals');
-    
     retJSON = JSON.parse(ret);
 
-    MyKRWBalance = await getBalance();
-    MyKRWBalance = JSON.parse(MyKRWBalance)[0].balance
+    MyWallet = await getBalance();
+    WalletJSON = JSON.parse(MyWallet)
+    MyKRWBalance = WalletJSON[0].balance
     console.log("your current KRW Balance is : " + MyKRWBalance) 
 
-    
-    
-    
-     while(MyKRWBalance > 0){ //돈없을때까지 무한동력
+    //body = await API_buyImmediate("KRW-MVL", 10000000); 
+
+    TotalCoinValue = 0;
+    CoinValue = 0;
+    // console.log(MyWallet)
+    for(var i = 1 in WalletJSON){ // 현재 코인가치 출력
+            MyCoin = WalletJSON[i].balance
+            MyValue = WalletJSON[i].avg_buy_price
+            //MyVal = retJSON["KRW-" + WalletJSON[i].currency].ask_price
+            CoinValue = MyCoin * MyValue
+            console.log(WalletJSON[i].currency + " : " + CoinValue + "\n")
+            TotalCoinValue += CoinValue  
+    }
+
+    // for(i in retJSON){ // testing 
+    //     console.log(retJSON[i].ask_price)
+    // }
+
+    //  while(MyKRWBalance > 0){ //돈없을때까지 무한동력
         for (var i in retJSON) {
             market = i;
             rsiSignal = retJSON[i].rsiSignal
-            if (rsiSignal == "LONG" || rsiSignal == "BIGLONG") {
-                if(MyKRWBalance > 5000000){ // 현금 500만원까지 유지
+            bidpower = retJSON[i].bid_power // 매수파워
+            askpower = retJSON[i].ask_power // 매도파워
+            if (MyKRWBalance > 25000000) {  // 현금 2500만원까지 유지
+                if(rsiSignal == "LONG" || rsiSignal == "BIGLONG"){ // 롱,빅롱포지션일때만 구매
                     console.log("자 드가자 : " + market);
-                    body = await API_buyImmediate(market, 100000);
+                    body = await API_buyImmediate(market, 1000000);
                     volume[market] = body.volume
+                    MyKRWBalance -= 1000000
+                    console.log("your current KRW Balance is : " + MyKRWBalance) 
+                } else if(bidpower > askpower){ // 매수힘이 더높으면 무지성으로 구매
+                    Super_Danta();
+                    MyKRWBalance -= 1000000
+                    console.log("your current KRW Balance is : " + MyKRWBalance) 
                 }
-            } else if (rsiSignal == "SHORT" || rsiSignal == "BIGSHORT") {
+            } else if (rsiSignal == "SHORT" || rsiSignal == "BIGSHORT") { // 숏,빅숏 포지션에서 매도
                 balance = await getBalance()
                 let volume;
                 for (var i in balance) {
                     if ("KRW-" + balance[i].currency == market) {
                         volume = balance[i].balance;
+                        if(volume > 0){
+                            console.log("돔황차~ : " + market)
+                            await API_sellImmediate(market, volume[market]);
+                        }
                     }
                 }
-                console.log("돔황차~ : " + market)
-                await API_sellImmediate(market, volume[market]);
+            } else if(bidpower<askpower){ // 매도힘이 더세면 매도
+                balance = await getBalance()
+                let volume;
+                for (var i in balance) {
+                    if ("KRW-" + balance[i].currency == market) {
+                        volume = balance[i].balance;
+                        if(volume > 0){
+                            console.log("하락장이다 돔황차~ : " + market)
+                            await API_sellImmediate(market, volume[market]);
+                        }
+                    }
+                }
+            }
+                // else if(TotalCoinValue + MyKRWBalance < 40000000){ // 현금 + 보유코인가치 4000만 이하면 다팔고 새시작
+                // await Armageddon();
+                // await LastGamble();
+                // }
             }
         }
-    }
-    MyCoinValue = await getBalance();
-    for(i = 1; i < getBalance.length ; i++){ // 현재 코인가치 출력
-        MyCoin = JSON.parse(MyCoinValue)[i].balance
-        MyValue = JSON.parse(MyCoinValue)[i].avg_buy_price
-        CoinValue = MyCoin * MyValue
-        console.log(CoinValue + "\n")
-        TotalCoinValue += CoinValue
-    }
-    if(TotalCoinValue + MyKRWBalance < 8000000){ // 현금 + 보유코인가치 800만 이하면 다팔고 새시작
-        await Armageddon();
-        await LastGamble();
-    }
-}
+
+    
+//}
 
 main()
-
-
-
-
-    // var access_key = "QNNtkaroxY3ASQrhxXXaAxuqJxFYtko61Y1xBPlW"
-    // var secret_key = "NW7dHskx9JQsHx4dOQM7i2RGuhG3qdmWBPkLnpxY"
-    // var server_url = "https://api.upbit.com"
-    // body = await getBalance(access_key, secret_key, server_url)
-    // console.log(body)
-    /*
-    [
-        {"currency":"KRW","balance":"몇개","locked":"11005499.93629343","avg_buy_price":"0","avg_buy_price_modified":true,"unit_currency":"KRW"},
-    ]
-    [
-        {"currency":"KRW","balance":"15232854.60024727","locked":"11005499.93629343","avg_buy_price":"0","avg_buy_price_modified":true,"unit_currency":"KRW"},
-        {"currency":"BTC","balance":"0.10344202","locked":"0.0","avg_buy_price":"48336245.83","avg_buy_price_modified":false,"unit_currency":"KRW"},
-        {"currency":"SNT","balance":"0.0","locked":"8227.02651414","avg_buy_price":"121.55","avg_buy_price_modified":false,"unit_currency":"KRW"},
-        {"currency":"ETH","balance":"1.3548063","locked":"0.0","avg_buy_price":"2952451.56","avg_buy_price_modified":false,"unit_currency":"KRW"},
-        {"currency":"ATOM","balance":"178.10340886","locked":"0.0","avg_buy_price":"16844.15","avg_buy_price_modified":false,"unit_currency":"KRW"},
-        {"currency":"BTT","balance":"0.0","locked":"914999.36839014","avg_buy_price":"5.56","avg_buy_price_modified":false,"unit_currency":"KRW"},
-        {"currency":"GAS","balance":"0.07329744","locked":"0.0","avg_buy_price":"14408.68","avg_buy_price_modified":false,"unit_currency":"KRW"},
-        {"currency":"WIN","balance":"730.56498603","locked":"0.0","avg_buy_price":"0","avg_buy_price_modified":false,"unit_currency":"KRW"},
-        {"currency":"META","balance":"0.0","locked":"25021.75517441","avg_buy_price":"116.29","avg_buy_price_modified":false,"unit_currency":"KRW"}
-    ]
-   
